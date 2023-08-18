3b39e987741cd82f30528d6f16d8df0b8d8d7904
Mark Adolf C. Ferolino <kramferolino@gmail.com>
2023-08-18 14:37:51 +0800

    Added a comment section for every blogs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_post
 
     def create 
-        @post = Post.find(params[:post_id])
-        @comment = @post.comment.create(comment_params)
+        @comment = @post.comments.create(comment_params)
         @comment.user = current_user 
 
         if @comment.save 
@@ -16,9 +16,16 @@ class CommentsController < ApplicationController
     end
     
     def destroy
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
     end
 
     private 
+
+    def set_post
+        @post = Post.find(params[:post_id])
+    end
 
     def comment_params 
         params.require(:comment).permit(:body)
--- db/migrate/20230817093745_change_views_for_users.rb
+++ db/migrate/20230817093745_change_views_for_users.rb
@@ -1,5 +1,5 @@
 class ChangeViewsForUsers < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :views, :integer, :default 0
+    change_column :users, :views, :integer, default: 0
   end
 end
