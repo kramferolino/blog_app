0776d9b16e67cfd81ce66dba18e6047f78538d56
Mark Adolf C. Ferolino <kramferolino@gmail.com>
2023-08-29 19:05:46 +0800

    Added search bar to search related blog posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   has_many :notifications, as: :recipient, dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "encrypted_password", "id", "name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at", "views"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["comments", "notifications", "posts"]
+  end
 end
