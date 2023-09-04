da20ff77970fcd9ddce73b539793437ff5a884ca
Mark Adolf C. Ferolino <kramferolino@gmail.com>
2023-09-04 18:52:09 +0800

    Switched the DB to postgres and fixed N+1 Query Fixes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -13,6 +13,6 @@ class AdminController < ApplicationController
   end
 
   def show_post
-    @post = Post.includes(:user, :comments).find(params[:id])
+    @post = Post.includes(:user, comments: [:user, :rich_text_body]).find(params[:id])
   end
 end
