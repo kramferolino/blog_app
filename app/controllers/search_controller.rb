fe94ceb18cb3a2b3df21cbd212f79ec8ac5fa201
Mark Adolf C. Ferolino <kramferolino@gmail.com>
2023-09-05 16:35:06 +0800

    Added .includes on post for search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @query = Post.ransack(params[:q])
+    @query = Post.includes(:user, :rich_text_body).ransack(params[:q])
     @posts = @query.result(distinct: true)
   end
 end
